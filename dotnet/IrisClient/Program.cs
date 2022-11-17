ed614afa34e739e076b0e5f0db0bf0de00acce62
Beth Thomas <beth.thomas@softwire.com>
2022-11-17 14:25:54 +0000

    Small code tidy up based on review comments

--- dotnet/IrisClient/Program.cs
+++ dotnet/IrisClient/Program.cs
@@ -15,25 +15,22 @@ var client = ServiceBusClientHelpers.GetAuthenticatedServiceBusClient(settings);
 var processor = client.CreateProcessor(settings.ServiceBusQueue);
 Console.WriteLine("Connection created with processor");
 
-if (settings.RelativeFileDownloadDirectory != null)
+try
 {
-    try
-    {
-        var handlers = new MessageProcessors(settings.RelativeFileDownloadDirectory);
-        processor.ProcessMessageAsync += handlers.DownloadHandler;
-        processor.ProcessErrorAsync += MessageProcessors.ErrorHandler;
-        await processor.StartProcessingAsync();
+    var handlers = new MessageProcessors(settings.RelativeFileDownloadDirectory!);
+    processor.ProcessMessageAsync += handlers.DownloadHandler;
+    processor.ProcessErrorAsync += MessageProcessors.ErrorHandler;
+    await processor.StartProcessingAsync();
 
-        Console.WriteLine("Wait for a minute and then press any key to end the processing");
-        Console.ReadKey();
+    Console.WriteLine("Wait for a minute and then press any key to end the processing");
+    Console.ReadKey();
 
-        Console.WriteLine("\nStopping the receiver...");
-        await processor.StopProcessingAsync();
-        Console.WriteLine("Stopped receiving messages");
-    }
-    finally
-    {
-        await processor.DisposeAsync();
-        await client.DisposeAsync();
-    }
+    Console.WriteLine("\nStopping the receiver...");
+    await processor.StopProcessingAsync();
+    Console.WriteLine("Stopped receiving messages");
+}
+finally
+{
+    await processor.DisposeAsync();
+    await client.DisposeAsync();
 }
