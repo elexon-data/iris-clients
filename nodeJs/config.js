95477d48ea8c12eb7955c8db6e80b6bb45f10b56
Beth Thomas <beth.thomas@softwire.com>
2022-12-16 10:51:41 +0000

    Refactor nodeJS settings

--- nodeJs/config.js
+++ nodeJs/config.js
@@ -1,5 +1,5 @@
 const serviceBusNamespace = process.env.SERVICE_BUS_NAMESPACE;
-const queueName = process.env.SERVICE_BUS_QUEUE;
+const queueName = process.env.QUEUE_NAME;
 const downloadDirectory = process.env.RELATIVE_FILE_DOWNLOAD_DIRECTORY
 const tenantId = "1a235385-5d29-40e1-96fd-bc5ec2706361"; // Elexon Tenant Id
 
@@ -8,22 +8,22 @@ if (!serviceBusNamespace) {
 }
 
 if (!queueName) {
-    throw new Error("Invalid configuration value: SERVICE_BUS_QUEUE is required");
+    throw new Error("Invalid configuration value: QUEUE_NAME is required");
 }
 
 if (!downloadDirectory) {
     throw new Error("Invalid configuration value: RELATIVE_FILE_DOWNLOAD_DIRECTORY is required");
 }
 
-if (!process.env.APP_REGISTRATION_CLIENT_ID !== !process.env.APP_REGISTRATION_SECRET) {
-    throw new Error("Invalid configuration value(s): If one of APP_REGISTRATION_CLIENT_ID and APP_REGISTRATION_CLIENT_SECRET is provided then both are required")
+if (!process.env.CLIENT_ID !== !process.env.SECRET) {
+    throw new Error("Invalid configuration value(s): If one of CLIENT_ID and SECRET is provided, both are required")
 }
 
 const appRegistration =
-  process.env.APP_REGISTRATION_CLIENT_ID && process.env.APP_REGISTRATION_SECRET
+  process.env.CLIENT_ID && process.env.SECRET
     ? {
-        clientId: process.env.APP_REGISTRATION_CLIENT_ID,
-        secret: process.env.APP_REGISTRATION_SECRET,
+        clientId: process.env.CLIENT_ID,
+        secret: process.env.SECRET,
       }
     : null;
 
